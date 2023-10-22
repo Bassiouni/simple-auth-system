573d99d3f68bc5a86917a7bcf98d8f08bf4c8c29
Muhammad Bassiouni <bassiouni.git@gmail.com>
2023-10-22 20:42:18 +0300

    adding helmet support

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
+  app.use(helmet());
 
-  console.log({ env: configService.get('ENV') })
+  console.log({ env: configService.get('ENV') });
   await app.listen(3000);
 }
 bootstrap();
